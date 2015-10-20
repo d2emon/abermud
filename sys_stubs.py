ee0983fd9aea54bc736394dab016cc95254e84bc
Dmitry Kutsenko <d2emonium@gmail.com>
2015-10-21 00:45:31 +0300

    New stubs

--- sys_stubs.py
+++ sys_stubs.py
@@ -25,6 +25,48 @@
 
 ttyt = 0
 
+qnmrq = False
 
 def clear_console():
     print('-=' * 80)
+
+
+def crapup(msg):
+    import sys
+
+    clear_console()
+    input("\n%s\n\nHit Return to Continue...\n" % (msg))
+    sys.exit(1)
+
+
+def elapsed(delta):
+    """
+    Elapsed time and similar goodies
+    """
+    if delta.days > 1:
+        return "Over a day!!!"
+
+    parts = []
+    hour = 60 * 60
+    hours = delta.seconds // hour
+    seconds = delta.seconds % hour
+    minutes = seconds // (60)
+
+    if hours > 1:
+        parts.append("%d hours" % (hours))
+    elif hours == 1:
+        parts.append("1 hour")
+
+    if minutes > 1:
+        parts.append("%d minutes" % (minutes))
+        return " and ".join(parts)
+    elif minutes == 1:
+        parts.append("1 minute")
+
+    seconds = (delta.seconds % 60)
+    if seconds > 1:
+        parts.append("%d seconds" % (seconds))
+    elif seconds == 1:
+        parts.append("1 second")
+
+    return " and ".join(parts)
