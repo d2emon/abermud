4a68ee0d10068b1e8277526d10f11c82c46322c2
Dmitry Kutsenko <d2emonium@gmail.com>
2019-10-11 17:56:59 +0300

    Add weather stub

---
+++ src/stubs/weather.py
@@ -0,0 +1,158 @@
+"""
+ The next part of the universe...
+"""
+
+"""
+Weather Routines
+
+Current weather defined by state of object 47
+
+states are
+
+0   Sunny
+1   Rain
+2   Stormy
+3   Snowing
+"""
+state = {
+    'hasfarted': 0,
+}
+
+
+def setwthr(n):
+    raise NotImplementedError()
+
+
+def suncom():
+    raise NotImplementedError()
+
+
+def raincom():
+    raise NotImplementedError()
+
+
+def stormcom():
+    raise NotImplementedError()
+
+
+def snowcom():
+    raise NotImplementedError()
+
+
+def blizzardcom():
+    raise NotImplementedError()
+
+
+def adjwthr(n):
+    raise NotImplementedError()
+
+
+def longwthr():
+    raise NotImplementedError()
+
+
+def wthrrcv(__type):
+    raise NotImplementedError()
+
+
+def showwthr():
+    raise NotImplementedError()
+
+
+def outdoors():
+    raise NotImplementedError()
+
+
+# Silly Section
+
+
+def sillycom(txt):
+    raise NotImplementedError()
+
+
+def laughcom():
+    raise NotImplementedError()
+
+
+def purrcom():
+    raise NotImplementedError()
+
+
+def crycom():
+    raise NotImplementedError()
+
+
+def sulkcom():
+    raise NotImplementedError()
+
+
+def burpcom():
+    raise NotImplementedError()
+
+
+def hiccupcom():
+    raise NotImplementedError()
+
+
+def fartcom():
+    raise NotImplementedError()
+
+
+def grincom():
+    raise NotImplementedError()
+
+
+def smilecom():
+    raise NotImplementedError()
+
+
+def winkcom():
+    raise NotImplementedError()
+
+
+def sniggercom():
+    raise NotImplementedError()
+
+
+def posecom():
+    raise NotImplementedError()
+
+
+def emotecom():
+    raise NotImplementedError()
+
+
+def praycom():
+    raise NotImplementedError()
+
+
+def yawncom():
+    raise NotImplementedError()
+
+
+def groancom():
+    raise NotImplementedError()
+
+
+def moancom():
+    raise NotImplementedError()
+
+
+def cancarry(plyr):
+    raise NotImplementedError()
+
+
+def setcom():
+    raise NotImplementedError()
+
+
+def isdark():
+    raise NotImplementedError()
+
+
+def modifwthr(n):
+    raise NotImplementedError()
+
+
+def setpflags():
+    raise NotImplementedError()
