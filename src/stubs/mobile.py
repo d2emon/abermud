a477aec108c1309b7b4adab0c95bb1dd6a16632a
Dmitry Kutsenko <d2emonium@gmail.com>
2019-10-14 10:02:35 +0300

    Add mobiles stub

---
+++ src/stubs/mobile.py
@@ -0,0 +1,53 @@
+def on_timing():
+    raise NotImplementedError()
+
+
+def onlook():
+    raise NotImplementedError()
+
+
+def chkfight(x):
+    raise NotImplementedError()
+
+
+def consid_move(x):
+    raise NotImplementedError()
+
+
+def crashcom():
+    raise NotImplementedError()
+
+
+def singcom():
+    raise NotImplementedError()
+
+
+def spraycom():
+    raise NotImplementedError()
+
+
+# More new stuff
+
+
+def dircom():
+    raise NotImplementedError()
+
+
+def sys_reset():
+    raise NotImplementedError()
+
+
+def dorune():
+    raise NotImplementedError()
+
+
+def pepdrop():
+    raise NotImplementedError()
+
+
+def dragget():
+    raise NotImplementedError()
+
+
+def helpchkr():
+    raise NotImplementedError()
